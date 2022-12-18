9c213d49f9422d97a488393b0ec5ca6f8c4c65d7
ozen-hub <info.ozenhub@gmail.com>
2022-12-18 09:24:43 +0530

    4

--- module-01/src/LoginFormController.java
+++ module-01/src/LoginFormController.java
@@ -1,12 +1,20 @@
 import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginFormController {
     public TextField txtUserName;
     public PasswordField txtPassword;
+    public CheckBox cmbRememberMe;
 
     public void btnLoginOnAction(ActionEvent actionEvent) {
         System.out.println("Username : "+txtUserName.getText()+" - Password : "+txtPassword.getText());
+        /*if (cmbRememberMe.isSelected()){
+            System.out.println("Remember me state is ON");
+        }else{
+            System.out.println("Remember me state is OFF");
+        }*/
+        System.out.println(cmbRememberMe.isSelected()?"Remember me state is ON":"Remember me state is OFF");
     }
 }
