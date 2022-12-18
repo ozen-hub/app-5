eec4002e6b6a3eb248f172a0042488be738eb0d4
ozen-hub <info.ozenhub@gmail.com>
2022-12-18 09:11:09 +0530

    2

--- module-01/src/LoginFormInitializer.java
+++ module-01/src/LoginFormInitializer.java
@@ -1,5 +1,9 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormInitializer extends Application {
 
@@ -8,7 +12,9 @@ public class LoginFormInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("loginForm.fxml"))));
+        primaryStage.show();
     }
 }
