11259519a0f5426cfa26cfec883614c6c7c992c4
ozen-hub <info.ozenhub@gmail.com>
2022-12-18 09:17:02 +0530

    3

--- module-01/src/LoginFormController.java
+++ module-01/src/LoginFormController.java
@@ -1,2 +1,12 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginFormController {
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+        System.out.println("Username : "+txtUserName.getText()+" - Password : "+txtPassword.getText());
+    }
 }
